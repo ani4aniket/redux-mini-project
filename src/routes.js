233796ad88d4db475a1b56890a19f1e7c2f926a4
Aniket Kumar <aniketkumar049@gmail.com>
2019-11-11 04:33:18 +0530

    first commit

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import Artist from "./components/Artist";
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path="/artist/:id" component={Artist} />
+      <Route path="/" component={Home} />
+    </Switch>
+  );
+};
+
+export default Routes;
